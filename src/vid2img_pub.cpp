29a1448e38b643b208c06355a5a8484423c47961
Mike Wiznitzer <mechanic.wiz@gmail.com>
2019-03-07 00:34:51 -0600

    modified first node to incorporate Gaussian blur window size from service

--- src/vid2img_pub.cpp
+++ src/vid2img_pub.cpp
@@ -3,8 +3,21 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include "tag_detector/WindowSize.h"
+#include <iostream>
 
+using namespace cv;
 // static const std::string OPENCV_WINDOW = "Image window";
+static int window_w = 0;
+static int window_h = 0;
+
+bool set_window_size(tag_detector::WindowSize::Request  &req,
+                     tag_detector::WindowSize::Response &res)
+{
+  window_w = req.w;
+  window_h = req.h;
+  return true;
+}
 
 int main(int argc, char** argv)
 {
@@ -13,29 +26,45 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
   image_transport::Publisher pub = it.advertise("image_raw", 1);
+  ros::ServiceServer service = nh.advertiseService("set_blur_window_size", set_window_size);
+
+  ROS_INFO("Waiting for GaussianBlur service call...");
+  ros::Rate loop_rate(30);
+  while (nh.ok()){
+    if (window_w > 0 && window_h > 0 && window_w % 2 == 1 && window_h % 2 == 1)
+      break;
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  auto kernel = Size(window_w,window_h);
 
   const std::string video_source = ros::package::getPath("tag_detector")+"/vid/tag_vid.MOV";
 
-  cv::VideoCapture cap(video_source);
+  VideoCapture cap(video_source);
 
   // Check if video device can be opened with the given name
   if(!cap.isOpened()) return 1;
-  cv::Mat frame;
+  Mat frame;
   sensor_msgs::ImagePtr msg;
 
-  // cv::namedWindow(OPENCV_WINDOW);
-  ros::Rate loop_rate(30);
+  // namedWindow(OPENCV_WINDOW);
   while (nh.ok()) {
     cap >> frame;
     // Check if grabbed frame is actually full with some content
     if(!frame.empty()) {
-      cv::resize(frame, frame, cv::Size(640, 480), 0, 0, CV_INTER_CUBIC);
-      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-      // cv::imshow(OPENCV_WINDOW, frame);
+      resize(frame, frame, Size(640, 480), 0, 0, CV_INTER_CUBIC);
+      Mat blurr;
+      if ((kernel.height != window_h || kernel.width != window_w) && window_h > 0 && window_h % 2 == 1 && window_w > 0 && window_w % 2 == 1){
+        kernel.height = window_h;
+        kernel.width = window_w;
+      }
+      GaussianBlur(frame,blurr,kernel,0);
+      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", blurr).toImageMsg();
+      // imshow(OPENCV_WINDOW, blurr);
       pub.publish(msg);
-      // cv::waitKey(1);
+      // waitKey(1);
     } else {
-      // cv::destroyAllWindows();
+      // destroyAllWindows();
       break;
     }
 
