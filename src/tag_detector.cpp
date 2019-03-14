b4a9a9ddb5cdac85046fedae1a499792ffdc707e
Mike Wiznitzer <mechanic.wiz@gmail.com>
2019-03-13 23:32:17 -0500

    converted to using classes

--- src/tag_detector.cpp
+++ src/tag_detector.cpp
@@ -10,231 +10,251 @@
 using namespace cv;
 using namespace std;
 
-static ros::ServiceClient client;
-static bool deblur_corr;
-static bool illum_corr;
+class TagDetector
+{
+private:
+  ros::NodeHandle nh;
+  image_transport::Subscriber sub;
+  image_transport::ImageTransport it;
+  ros::ServiceClient client;
+  bool deblur_corr;
+  bool illum_corr;
+  int window_w;
 
-vector<Mat> fft(Mat &I,int i_row, int i_col){
-  // function based off of opencv tutorial
-  // https://docs.opencv.org/3.4/d8/d01/tutorial_discrete_fourier_transform.html
+public:
+  TagDetector()
+  : it(nh)
+  {
+    ros::param::get("tag_detector/deblur_correction",deblur_corr);
+    ros::param::get("tag_detector/illumination_correction",illum_corr);
 
-//! [expand]
-    Mat padded;                            //expand input image to optimal size
-    int m = getOptimalDFTSize( i_row );
-    int n = getOptimalDFTSize( i_col ); // on the border add zero values
-    copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
-//! [expand]
+    sub = it.subscribe("image_raw", 1, &TagDetector::imageCallback,this);
+    client = nh.serviceClient<tag_detector::WindowSize>("set_blur_window_size",this);
+  }
 
-//! [complex_and_real]
-    Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
-    Mat complexI;
-    merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
-//! [complex_and_real]
+  ~TagDetector()
+  {
+    destroyAllWindows();
+  }
 
-//! [dft]
-    dft(complexI, complexI);            // this way the result may fit in the source matrix
-//! [dft]
-
-    // compute the magnitude and switch to logarithmic scale
-    // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
-//! [magnitude]
-    split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-    magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
-    Mat magI = planes[0];
-//! [magnitude]
-    vector<Mat> res = {complexI,magI};
-
-    return res;
-}
-
-int findSharpness(vector<Mat> &res){
+  vector<Mat> fft(Mat &I,int i_row, int i_col){
     // function based off of opencv tutorial
     // https://docs.opencv.org/3.4/d8/d01/tutorial_discrete_fourier_transform.html
 
-    Mat complexI = res.at(0);
-    Mat magI = res.at(1);
+  //! [expand]
+      Mat padded;                            //expand input image to optimal size
+      int m = getOptimalDFTSize( i_row );
+      int n = getOptimalDFTSize( i_col ); // on the border add zero values
+      copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
+  //! [expand]
 
-//! [log]
-    magI += Scalar::all(1);                    // switch to logarithmic scale
-    log(magI, magI);
-//! [log]
+  //! [complex_and_real]
+      Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+      Mat complexI;
+      merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+  //! [complex_and_real]
 
-//! [crop_rearrange]
-    // crop the spectrum, if it has an odd number of rows or columns
-    magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
+  //! [dft]
+      dft(complexI, complexI);            // this way the result may fit in the source matrix
+  //! [dft]
 
-    // rearrange the quadrants of Fourier image  so that the origin is at the image center
-    int cx = magI.cols/2;
-    int cy = magI.rows/2;
+      // compute the magnitude and switch to logarithmic scale
+      // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
+  //! [magnitude]
+      split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+      magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+      Mat magI = planes[0];
+  //! [magnitude]
+      vector<Mat> res = {complexI,magI};
 
-    Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-    Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
-    Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
-    Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
+      return res;
+  }
 
-    Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
-    q0.copyTo(tmp);
-    q3.copyTo(q0);
-    tmp.copyTo(q3);
+  int findSharpness(vector<Mat> &res){
+      // function based off of opencv tutorial
+      // https://docs.opencv.org/3.4/d8/d01/tutorial_discrete_fourier_transform.html
 
-    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
-    q2.copyTo(q1);
-    tmp.copyTo(q2);
-//! [crop_rearrange]
+      Mat complexI = res.at(0);
+      Mat magI = res.at(1);
 
-//! [normalize]
-    normalize(magI, magI, 0, 1, NORM_MINMAX);  // Transform the matrix with float values into a
-                                              // viewable image form (float between values 0 and 1)
-    // imshow("spectrum magnitude", magI);
+  //! [log]
+      magI += Scalar::all(1);                    // switch to logarithmic scale
+      log(magI, magI);
+  //! [log]
 
-//! [threshold]
-    threshold(magI,magI, 0.2, 1, THRESH_BINARY);
+  //! [crop_rearrange]
+      // crop the spectrum, if it has an odd number of rows or columns
+      magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
 
-    erode(magI,magI,Mat(),Point(-1,1));
+      // rearrange the quadrants of Fourier image  so that the origin is at the image center
+      int cx = magI.cols/2;
+      int cy = magI.rows/2;
 
-//! [sharpness]
-    double sharpness = countNonZero(magI)/static_cast<double>(magI.rows*magI.cols);
+      Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+      Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
+      Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
+      Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
 
-    return static_cast<int>(sharpness*100);
-}
+      Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
+      q0.copyTo(tmp);
+      q3.copyTo(q0);
+      tmp.copyTo(q3);
 
-Mat deblur(Mat &complexI,int ksize, int i_row, int i_col){
-  // implemented based off of http://www.owlnet.rice.edu/~elec539/Projects99/BACH/proj2/inverse.html ---> still needs work
-  Mat kw = getGaussianKernel(ksize,0);//
-  Mat k = kw*kw.t();
-  vector<Mat> res = fft(k,i_row,i_col);
-  Mat kernel = res.at(0);
-  Mat r;
-  // kernel.setTo(0.01,abs(kernel)<0.01);
-  divide(complexI,kernel,r);
-  Mat inverseTransform;
-  idft(r,inverseTransform,DFT_REAL_OUTPUT);
-  normalize(inverseTransform, inverseTransform, 0, 1, NORM_MINMAX);
-  // inverseTransform.convertTo(inverseTransform, CV_8U);
-  // imshow("Reconstructed", inverseTransform);
-  return inverseTransform;
-}
+      q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+      q2.copyTo(q1);
+      tmp.copyTo(q2);
+  //! [crop_rearrange]
 
-void correctIllumination(Mat &bgr_image, Mat &result){
-  // implemented from https://stackoverflow.com/questions/24341114/simple-illumination-correction-in-images-opencv-c
-  // still could be better implemented for this application
+  //! [normalize]
+      normalize(magI, magI, 0, 1, NORM_MINMAX);  // Transform the matrix with float values into a
+                                                // viewable image form (float between values 0 and 1)
+      // imshow("spectrum magnitude", magI);
 
-  Mat lab_image;
-  cvtColor(bgr_image, lab_image, CV_BGR2Lab);
+  //! [threshold]
+      threshold(magI,magI, 0.2, 1, THRESH_BINARY);
 
-  // Extract the L channel
-  vector<Mat> lab_planes(3);
-  split(lab_image, lab_planes);  // now we have the L image in lab_planes[0]
+      erode(magI,magI,Mat(),Point(-1,1));
 
-  // apply the CLAHE algorithm to the L channel
-  Ptr<CLAHE> clahe = createCLAHE();
-  clahe->setClipLimit(4);
-  Mat dst;
-  clahe->apply(lab_planes[0], dst);
+  //! [sharpness]
+      double sharpness = countNonZero(magI)/static_cast<double>(magI.rows*magI.cols);
 
-  // Merge the the color planes back into an Lab image
-  dst.copyTo(lab_planes[0]);
-  merge(lab_planes, lab_image);
+      return static_cast<int>(sharpness*100);
+  }
 
- // convert back to RGB
- Mat image_clahe;
- cvtColor(lab_image, image_clahe, CV_Lab2BGR);
- result = image_clahe;
-}
+  Mat deblur(Mat &complexI,int ksize, int i_row, int i_col){
+    // implemented based off of http://www.owlnet.rice.edu/~elec539/Projects99/BACH/proj2/inverse.html ---> still needs work
+    Mat kw = getGaussianKernel(ksize,0);//
+    Mat k = kw*kw.t();
+    vector<Mat> res = fft(k,i_row,i_col);
+    Mat kernel = res.at(0);
+    Mat r;
+    // kernel.setTo(0.01,abs(kernel)<0.01);
+    divide(complexI,kernel,r);
+    Mat inverseTransform;
+    idft(r,inverseTransform,DFT_REAL_OUTPUT);
+    normalize(inverseTransform, inverseTransform, 0, 1, NORM_MINMAX);
+    // inverseTransform.convertTo(inverseTransform, CV_8U);
+    // imshow("Reconstructed", inverseTransform);
+    return inverseTransform;
+  }
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-  cv_bridge::CvImageConstPtr cv_ptr;
-  static vector<chrono::microseconds> times;
-  static vector<int> sharps;
-  static int num_rect = 35;
-  tag_detector::WindowSize srv;
-  srv.request.w = 0;
+  void correctIllumination(Mat &bgr_image, Mat &result){
+    // implemented from https://stackoverflow.com/questions/24341114/simple-illumination-correction-in-images-opencv-c
+    // still could be better implemented for this application
 
-  try
+    Mat lab_image;
+    cvtColor(bgr_image, lab_image, CV_BGR2Lab);
+
+    // Extract the L channel
+    vector<Mat> lab_planes(3);
+    split(lab_image, lab_planes);  // now we have the L image in lab_planes[0]
+
+    // apply the CLAHE algorithm to the L channel
+    Ptr<CLAHE> clahe = createCLAHE();
+    clahe->setClipLimit(4);
+    Mat dst;
+    clahe->apply(lab_planes[0], dst);
+
+    // Merge the the color planes back into an Lab image
+    dst.copyTo(lab_planes[0]);
+    merge(lab_planes, lab_image);
+
+   // convert back to RGB
+   Mat image_clahe;
+   cvtColor(lab_image, image_clahe, CV_Lab2BGR);
+   result = image_clahe;
+  }
+
+  void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
-    cv_ptr = cv_bridge::toCvShare(msg, "bgr8");
-    // imshow("view", cv_ptr->image);
-    Mat gray, dst, edge, draw;
-    dst = cv_ptr->image;
+    cv_bridge::CvImageConstPtr cv_ptr;
+    static vector<chrono::microseconds> times;
+    static vector<int> sharps;
+    static int num_rect = 35;
+    tag_detector::WindowSize srv;
+    srv.request.w = 0;
 
-    // Note, illumination correction could still used some work
-    if (illum_corr){
-      correctIllumination(dst,dst);
-      // imshow("illum",dst);
-    }
-    cvtColor(dst, gray, CV_BGR2GRAY);
-    vector<Mat> res = fft(gray,gray.rows,gray.cols);
-    int sharpness = findSharpness(res);
+    try
+    {
+      cv_ptr = cv_bridge::toCvShare(msg, "bgr8");
+      // imshow("view", cv_ptr->image);
+      Mat gray, dst, edge, draw;
+      dst = cv_ptr->image;
 
-    // Note, deblurring code is not functioning correctly yet
-    if (sharpness < 50 && num_rect < 35 && deblur_corr){
-      client.call(srv);
-      int winSize = static_cast<int>(srv.response.w);
-      Mat sharp_image = deblur(res.at(0),srv.response.w,gray.rows,gray.cols);
-      // imshow("deblur",sharp_image);
-    }
+      // Note, illumination correction could still used some work
+      if (illum_corr){
+        correctIllumination(dst,dst);
+        // imshow("illum",dst);
+      }
+      cvtColor(dst, gray, CV_BGR2GRAY);
+      vector<Mat> res = fft(gray,gray.rows,gray.cols);
+      int sharpness = findSharpness(res);
 
-    auto start = chrono::high_resolution_clock::now();
-    Canny( gray, edge, 50, 150, 3);
-    dilate(edge,edge,Mat(),Point(-1,-1));
-    edge.convertTo(draw, CV_8U);
-    // imshow("canny",draw);
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
+      // Note, deblurring code is not functioning correctly yet
+      if (sharpness < 50 && num_rect < 35 && deblur_corr){
+        client.call(srv);
+        int winSize = static_cast<int>(srv.response.w);
+        Mat sharp_image = deblur(res.at(0),srv.response.w,gray.rows,gray.cols);
+        // imshow("deblur",sharp_image);
+      }
 
-    findContours(draw,contours,hierarchy,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+      auto start = chrono::high_resolution_clock::now();
+      Canny( gray, edge, 50, 150, 3);
+      dilate(edge,edge,Mat(),Point(-1,-1));
+      edge.convertTo(draw, CV_8U);
+      // imshow("canny",draw);
+      vector<vector<Point> > contours;
+      vector<Vec4i> hierarchy;
 
-    Rect boundRect;
-    double ar;
+      findContours(draw,contours,hierarchy,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-    vector<Point> approxSquare;
-    num_rect = 0;
-    for(size_t i = 0; i < contours.size(); i++){
-        approxPolyDP(contours[i], approxSquare, arcLength(Mat(contours[i]), true)*0.02, true);
-        if(approxSquare.size() == 4 && isContourConvex(approxSquare) && contourArea(approxSquare)>100){
-          boundRect = boundingRect( approxSquare );
-          ar = boundRect.width/static_cast<double>(boundRect.height);
-          if (ar>0.5 && ar <1.5){
-            drawContours(cv_ptr->image, contours, i, Scalar(0, 255, 0),3); // fill GREEN
-            num_rect++;
+      Rect boundRect;
+      double ar;
+
+      vector<Point> approxSquare;
+      num_rect = 0;
+      for(size_t i = 0; i < contours.size(); i++){
+          approxPolyDP(contours[i], approxSquare, arcLength(Mat(contours[i]), true)*0.02, true);
+          if(approxSquare.size() == 4 && isContourConvex(approxSquare) && contourArea(approxSquare)>100){
+            boundRect = boundingRect( approxSquare );
+            ar = boundRect.width/static_cast<double>(boundRect.height);
+            if (ar>0.5 && ar <1.5){
+              drawContours(cv_ptr->image, contours, i, Scalar(0, 255, 0),3); // fill GREEN
+              num_rect++;
+            }
           }
-        }
-    }
-    auto stop = chrono::high_resolution_clock::now();
-    imshow("overlay", cv_ptr->image);
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    if (times.size() <= 100)
-      times.push_back(duration);
-      sharps.push_back(sharpness);
+      }
+      auto stop = chrono::high_resolution_clock::now();
+      imshow("overlay", cv_ptr->image);
+      auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+      if (times.size() <= 100)
+        times.push_back(duration);
+        sharps.push_back(sharpness);
 
-    if (times.size() == 100){
-      chrono::microseconds sum;
-      for (auto &n : times)
-        sum += n;
-      auto avg = sum/times.size();
-      int sharp_avg = accumulate(sharps.begin(),sharps.end(),0.0)/sharps.size();
-      ROS_INFO("Average computation time taken by function over 100 calls: %ld microseconds",avg.count());
-      ROS_INFO("Average percent sharpness over 100 calls: %d%%",sharp_avg);
+      if (times.size() == 100){
+        chrono::microseconds sum;
+        for (auto &n : times)
+          sum += n;
+        auto avg = sum/times.size();
+        int sharp_avg = accumulate(sharps.begin(),sharps.end(),0.0)/sharps.size();
+        ROS_INFO("Average computation time taken by function over 100 calls: %ld microseconds",avg.count());
+        ROS_INFO("Average percent sharpness over 100 calls: %d%%",sharp_avg);
+      }
+      waitKey(3);
     }
-    waitKey(3);
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+    }
   }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-  }
-}
+
+};
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "image_listener");
-  ros::NodeHandle nh;
-  ros::param::get("tag_detector/deblur_correction",deblur_corr);
-  ros::param::get("tag_detector/illumination_correction",illum_corr);
-  image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("image_raw", 1, imageCallback);
-  client = nh.serviceClient<tag_detector::WindowSize>("set_blur_window_size");
-
+  TagDetector tg;
   ros::spin();
-  destroyAllWindows();
+  return 0;
 }
--- src/vid2img_pub.cpp
+++ src/vid2img_pub.cpp
@@ -7,76 +7,97 @@
 #include <iostream>
 
 using namespace cv;
-static int window_w = 0;
 
-bool set_window_size(tag_detector::WindowSize::Request  &req,
-                     tag_detector::WindowSize::Response &res)
+class ImagePublisher
 {
-  if (req.w == 0)
+private:
+  ros::NodeHandle nh;
+  image_transport::Publisher pub;
+  ros::ServiceServer service;
+  ros::Rate loop_rate;
+  image_transport::ImageTransport it;
+  int window_w;
+
+public:
+  ImagePublisher()
+  : it(nh), window_w(0), loop_rate(30)
   {
-    if (window_w == 0)
-      return false;
-    res.w = window_w;
-  }else{
-    if (req.w < 1 || req.w % 2 == 0)
-      return false;
-    window_w = req.w;
-    res.w = req.w;
+    pub = it.advertise("image_raw", 1);
+    service = nh.advertiseService("set_blur_window_size", &ImagePublisher::set_window_size, this);
+
+    ROS_INFO("Waiting for GaussianBlur service call...");
+
+    while (nh.ok()){
+      if (window_w > 0)
+        break;
+      ros::spinOnce();
+      loop_rate.sleep();
+    }
+    auto kernel = Size(window_w,window_w);
+
+    const std::string video_source = ros::package::getPath("tag_detector")+"/vid/tag_vid.MOV";
+
+    VideoCapture cap(video_source);
+
+    // Check if video device can be opened with the given name
+    if(cap.isOpened())
+    {
+      Mat frame;
+      sensor_msgs::ImagePtr msg;
+
+      while (nh.ok()) {
+        cap >> frame;
+        // Check if grabbed frame is actually full with some content
+        if(!frame.empty()) {
+          resize(frame, frame, Size(640, 480), 0, 0, CV_INTER_CUBIC);
+          Mat blurr;
+          if (kernel.width != window_w){
+            kernel.height = window_w;
+            kernel.width = window_w;
+          }
+          GaussianBlur(frame,blurr,kernel,0);
+          msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", blurr).toImageMsg();
+          // imshow("blurred image", blurr);
+          pub.publish(msg);
+          // waitKey(1);
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
+      }
+    }
   }
 
-  return true;
-}
+  ~ImagePublisher()
+  {
+    destroyAllWindows();
+  }
+
+  bool set_window_size(tag_detector::WindowSize::Request  &req,
+                       tag_detector::WindowSize::Response &res)
+  {
+    if (req.w == 0)
+    {
+      if (window_w == 0)
+        return false;
+      res.w = window_w;
+    }else{
+      if (req.w < 1 || req.w % 2 == 0)
+        return false;
+      window_w = req.w;
+      res.w = req.w;
+    }
+
+    return true;
+  }
+};
+
 
 int main(int argc, char** argv)
 {
 
   ros::init(argc, argv, "image_publisher");
-  ros::NodeHandle nh;
-  image_transport::ImageTransport it(nh);
-  image_transport::Publisher pub = it.advertise("image_raw", 1);
-  ros::ServiceServer service = nh.advertiseService("set_blur_window_size", set_window_size);
-
-  ROS_INFO("Waiting for GaussianBlur service call...");
-  ros::Rate loop_rate(30);
-  while (nh.ok()){
-    if (window_w > 0)
-      break;
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-  auto kernel = Size(window_w,window_w);
-
-  const std::string video_source = ros::package::getPath("tag_detector")+"/vid/tag_vid.MOV";
-
-  VideoCapture cap(video_source);
-
-  // Check if video device can be opened with the given name
-  if(!cap.isOpened()) return 1;
-  Mat frame;
-  sensor_msgs::ImagePtr msg;
-
-  while (nh.ok()) {
-    cap >> frame;
-    // Check if grabbed frame is actually full with some content
-    if(!frame.empty()) {
-      resize(frame, frame, Size(640, 480), 0, 0, CV_INTER_CUBIC);
-      Mat blurr;
-      if (kernel.width != window_w){
-        kernel.height = window_w;
-        kernel.width = window_w;
-      }
-      GaussianBlur(frame,blurr,kernel,0);
-      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", blurr).toImageMsg();
-      // imshow("blurred image", blurr);
-      pub.publish(msg);
-      // waitKey(1);
-    } else {
-      // destroyAllWindows();
-      break;
-    }
-
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-  ros::shutdown();
+  ImagePublisher ip;
+  ros::spin();
+  return 0;
 }
